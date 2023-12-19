5dea89417e0941d7324103807a57ed5ead2bbb32
Jason Lee <jal004@ucsd.edu>
2023-12-18 18:20:07 -0800

    day 6 part 2

--- solutions/Day6/Main.java
+++ solutions/Day6/Main.java
@@ -16,11 +16,12 @@ public class Main {
         String content = new String(Files.readAllBytes(Paths.get(inputUri)));
 
         System.out.println("The answer to Problem 1 is: " + new Problem1().waysProduct(content));
-        //System.out.println("The answer to Problem 2 is: " + new Problem2().solution(content));
+        System.out.println("The answer to Problem 2 is: " + new Problem2().computeWays(content));
     }
 }
 
 class Problem1 {
+    // brute force approach
     public int waysProduct(String content) {
         int result = 1;
         String[] sections = content.split("\\r?\\n");
@@ -55,6 +56,40 @@ class Problem1 {
     }
 }
 
-//class Problem2 {
-//
-//}
+class Problem2 {
+    // quadratic solution
+    public long computeWays(String content) {
+        String[] sections = content.split("\\r?\\n");
+
+        List<String> times = new LinkedList<>(Arrays.asList(sections[0].split("\\s+")));
+        times.remove(0);
+
+        List<String> distances = new LinkedList<>(Arrays.asList(sections[1].split("\\s+")));
+        distances.remove(0);
+
+        StringBuilder timeSB = new StringBuilder();
+        StringBuilder distanceSB = new StringBuilder();
+
+        for (String time : times) {
+            timeSB.append(time);
+        }
+
+        for (String dist : distances) {
+            distanceSB.append(dist);
+        }
+
+        long trueTime = Long.parseLong(timeSB.toString());
+        long trueDist = Long.parseLong(distanceSB.toString());
+
+        // solve for integers h s.t. h(T - h) >= D; where T and D are constant ints for trueTime and trueDists
+        // => find integers h s.t. h^2 -Th + D <= 0
+        //    h >= [T - sqrt(T^2 - 4D)] / 2 (lower bound; take ceil)
+        //    h <= [T + sqrt(T^2 - 4D)] / 2 (upper bound; take floor)
+
+        double sqrt = Math.sqrt(Math.pow(trueTime, 2) - 4 * trueDist);
+        long lowerBound = (long) Math.ceil((trueTime - sqrt) / 2);
+        long upperBound = (long) Math.floor((trueTime + sqrt) / 2);
+
+        return upperBound - lowerBound + 1;
+    }
+}
